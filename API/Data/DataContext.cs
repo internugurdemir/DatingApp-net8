db3101fa067814e50df7ed7e0bd9eff63a68db37
Uğur Demir <ardahan1984@hotmail.com>
2024-09-21 06:50:21 +0300

    End of Section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+using System;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+// public class DataContext: DbContext
+// {
+//         public DataContext(DbContextOptions<DataContext> options) : base (options)
+//         {
+            
+//         }
+
+// }
+
+public class DataContext(DbContextOptions options): DbContext(options){
+
+        public DbSet<AppUser> Users {get; set;}
+}
