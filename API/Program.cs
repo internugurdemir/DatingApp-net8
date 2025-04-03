9046ed2bb6ee4ec89f1388c403c365d409d1eb5a
Uğur Demir <ardahan1984@hotmail.com>
2025-04-03 15:40:41 +0300

    CORS-Cert_ssl

--- API/Program.cs
+++ API/Program.cs
@@ -10,15 +10,18 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
+app.UseCors(a=>a.AllowAnyHeader().AllowAnyMethod()
+        .WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
